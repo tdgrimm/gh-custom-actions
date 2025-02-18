a2ccf3bc2c114eaa6d4c639c54dab2d19440e4db
Terry Grimm <Terry.Grimm@neudesic.com>
2025-02-18 10:11:57 -0600

    added custom deploy to aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
-    core.notice('Hello from custom Javascript action!');
+    // 1) Get the inputs
+    const bucket = core.getInput('bucket-name', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // 2) Upload files
+    const s3URI = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
+
+    core.notice('Deploy complete!');
 }
 
 run();
